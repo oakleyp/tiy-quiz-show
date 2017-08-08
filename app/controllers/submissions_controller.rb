ec90529be17c1897e692c0077b5f3c84455e6b69
oakley <oakleypeavler@gmail.com>
2017-08-07 20:05:54 -0400

    Fix admin and user_dash gradebook, add quiz taking, quiz grading

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,7 +1,7 @@
 class SubmissionsController < ApplicationController
   before_action :require_user
   before_action :require_admin, only: [:show, :index]
-  before_action :require_no_open_submissions, only: :create
+  #before_action :require_no_open_submissions, only: :create
   before_action :set_submission, only: [:show, :edit, :update, :destroy]
 
   # New is implemented in quizzes#show method
@@ -15,12 +15,12 @@ class SubmissionsController < ApplicationController
     @quiz = Quiz.find(@submission[:quiz_id])
 
     if(@quiz && @submission)
-      qa_hash = json_to_hasharray(@quiz.qajson)
+      @qa_hash = json_to_hasharray(@quiz.qajson)
       @submission[:user_id] = current_user.id
-      @submission[:qajson] = ''
+      @submission[:qajson] = @quiz.qajson
       @submission[:complete] = false
       @submission[:correct] = 0
-      @submission[:possible] = qa_hash.count if qa_hash
+      @submission[:possible] = @qa_hash.count if @qa_hash
 
       if(@submission.save)
         render :edit
@@ -34,10 +34,24 @@ class SubmissionsController < ApplicationController
 
   def edit
     @quiz = Quiz.find(@submission[:quiz_id])
-    @qa_hash = json_to_hasharray(@quiz.qajson)
+    #Shuffled here to keep edit template render and javascript scoring in tact
+    @qa_hash = json_to_hasharray(@submission.qajson).shuffle!
   end
 
   def update
+    @quiz = Quiz.find(@submission[:quiz_id])
+    if @submission.update(submission_params)
+      qajson_array = json_to_hasharray(@submission.qajson)
+      @submission[:correct] = count_json_correct(qajson_array)
+      @submission[:possible] = qajson_array.count
+      if @submission.save
+        render :show, notice: "Quiz has been submitted successfully."
+      else 
+        render :edit, notice: "Error submitting quiz."
+      end
+    else 
+      render :edit, notice: "Error submitting quiz."
+    end
   end
 
   def show
@@ -57,5 +71,17 @@ class SubmissionsController < ApplicationController
 
   def set_submission
     @submission = Submission.find(params[:id])
+  end
+
+  #Count number of correct answers in submission qajson
+  def count_json_correct(qajson_array)
+    result = 0
+    qajson_array.each do |hash|
+        p hash
+        printf("\n\n");
+        if hash["selection"] == hash["correct"] then result += 1 end
+    end
+    puts result
+    result
   end
 end
